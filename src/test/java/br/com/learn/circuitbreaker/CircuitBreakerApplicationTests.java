da569dd436877b84e4f1dff3437e4514cb503304
= <=>
2023-06-20 01:29:29 -0300

    init

---
+++ src/test/java/br/com/learn/circuitbreaker/CircuitBreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.learn.circuitbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
